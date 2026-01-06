c44e2206a54e1c04f66f896564a5bfe971f34324
Thomas Bright <brightotoms3@gmail.com>
2026-01-06 07:32:53 +0000

    Maybe a little something that will show what the app does

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      </head>
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
